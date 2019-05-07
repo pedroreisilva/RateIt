3bea0659951f2ab0c054ef68626225d94f6eaa05
pedroreisilva <48354011+pedroreisilva@users.noreply.github.com>
2019-05-07 18:49:41 +0100

    Added BdOpenHelper

--- app/src/main/java/pt/ipg/rateit/Categorias.java
+++ app/src/main/java/pt/ipg/rateit/Categorias.java
@@ -1,22 +1,50 @@
 package pt.ipg.rateit;
 
-public class Categorias {
-    private int id;
-    private String name;
+import android.content.ContentValues;
+import android.database.Cursor;
 
-    public int getId() {
+public class Categorias {
+    private long id;
+    private String categoria;
+
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getCategoria() {
+        return categoria;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public ContentValues getContentValues() {
+        ContentValues valores = new ContentValues();
+
+        valores.put(BdTableCategorias.CAMPO_CATEGORIA, categoria);
+
+        return valores;
+    }
+
+    public static Categorias fromCursor(Cursor cursor) {
+        long id = cursor.getLong(
+                cursor.getColumnIndex(BdTableCategorias._ID)
+        );
+
+        String categoria = cursor.getString(
+                cursor.getColumnIndex(BdTableCategorias.CAMPO_CATEGORIA)
+        );
+
+        Categorias categorias = new Categorias();
+
+        categorias.setId(id);
+        categorias.setCategoria(categoria);
+
+        return categorias;
     }
 }
