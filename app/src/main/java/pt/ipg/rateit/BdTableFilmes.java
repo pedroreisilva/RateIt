db8fc1eb9b7766803989727d3ad614e841e48617
pedroreisilva <48354011+pedroreisilva@users.noreply.github.com>
2019-06-06 18:51:39 +0100

    Changed filmes query to return category name

--- app/src/main/java/pt/ipg/rateit/BdTableFilmes.java
+++ app/src/main/java/pt/ipg/rateit/BdTableFilmes.java
@@ -4,14 +4,20 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
+import android.text.TextUtils;
+import android.util.Log;
 
 public class BdTableFilmes implements BaseColumns {
     public static final String NOME_TABELA = "Filmes";
+
+    public static final String ALIAS_NOME_CATEGORIA = "nome_categ";
     public static final String CAMPO_NOME = "Nome";
     public static final String CAMPO_CATEGORIA = "Categoria";
     public static final String CAMPO_NOTA = "Nota";
     public static final String CAMPO_DATA_VISUALIZACAO = "Data";
-    public static final String[] TODAS_COLUNAS = new String[] { _ID, CAMPO_NOME, CAMPO_CATEGORIA, CAMPO_NOTA, CAMPO_DATA_VISUALIZACAO};
+    public static final String CAMPO_NOME_CATEGORIA = BdTableCategorias.NOME_TABELA + "." + BdTableCategorias.CAMPO_GENERO + " AS " + ALIAS_NOME_CATEGORIA; // tabela de categorias (só de leitura)
+
+    public static final String[] TODAS_COLUNAS = new String[] { NOME_TABELA + "." + _ID, CAMPO_NOME, CAMPO_NOTA, CAMPO_DATA_VISUALIZACAO, CAMPO_CATEGORIA, CAMPO_NOME_CATEGORIA };
 
     private final SQLiteDatabase db;
 
@@ -34,7 +40,20 @@ public class BdTableFilmes implements BaseColumns {
 
     //CRUD
     public Cursor query(String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
-        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy);
+        String colunasSelect = TextUtils.join(",", columns);
+
+        String sql = "SELECT " + colunasSelect + " FROM " +
+                NOME_TABELA + " INNER JOIN " + BdTableCategorias.NOME_TABELA + " WHERE " + NOME_TABELA + "." + CAMPO_CATEGORIA + "=" + BdTableCategorias.NOME_TABELA + "." + BdTableCategorias._ID
+                ;
+
+        if (selection != null) {
+            sql += " AND " + selection;
+        }
+
+        Log.d("Tabela Livros", "query: " + sql);
+
+        return db.rawQuery(sql, selectionArgs);
+
     }
 
     public long insert(ContentValues values) {
